e246670c8bfd3f431c22332fec666b29e6347088
group2 <group2>
2025-11-10 10:15:08 +0100

    Debut parsing arguments

--- src/main.c
+++ src/main.c
@@ -1,16 +1,46 @@
 #include "led.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <wiringPi.h>
+#include <string.h>
 
 #define RED_PIN 22
 #define GREEN_PIN 27
 #define BLUE_PIN 17
 #define POWER_PIN 26
 
-int main() {
+int main(int argc, char *argv[]) {
     Led led;
     Color c = {132, 3, 252, 0};
+    int i;
+
+    char input[255] = {0};
+
+
+    FILE* fdi;
+    
+    for (i = 0; i < argc; i++) {
+	if (strcmp(argv[i], "--input") == 0) {
+	    if (i+1 >= argc) {
+	        printf("Mauvais usage: --input attend un argument\n");
+                exit(1);
+	    }
+	    strncpy(input, argv[i+1], 255);
+	}
+    }
+
+    printf("Debug: input=%s\n", input);
+
+    if (strlen(input) == 0) {
+        fdi = stdin;
+    } else {
+    	fdi = fopen(input, "r");
+	if (fdi == NULL) {
+	    printf("Erreur\n");
+	    exit(1);
+	}
+    }
 
     init_led(&led, RED_PIN, GREEN_PIN, BLUE_PIN, POWER_PIN);
 
