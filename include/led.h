4bfa503b06627cd3cb304a193ea7130ca1bc5581
group2 <group2>
2025-11-10 09:51:30 +0100

    Modification led.h

--- include/led.h
+++ include/led.h
@@ -8,11 +8,6 @@
 #define BLUE ((Color){0, 0, 255, 255})
 #define WHITE ((Color){255, 255, 255, 255})
 
-#define RED_PIN 22
-#define GREEN_PIN 27
-#define BLUE_PIN 17
-#define POWER_PIN 26
-
 typedef struct   Color {
         
         uint8_t  red;
