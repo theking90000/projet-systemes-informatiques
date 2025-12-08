03f8375a2d012b6d2ede4d29bc0c80b2a52b1b89
group2 <group2>
2025-12-08 09:48:03 +0100

    Déplacement de logique string dans cstring.c et cstring.h

--- include/math_suite.h
+++ include/math_suite.h
@@ -2,25 +2,12 @@
 #define MATH_SUITE
 
 #include <stdio.h>
-
-typedef struct  String {
-
-        char    *ptr;
-        size_t  max_size;
-
-}               String;
+#include "cstring.h"
 
 int solve(FILE*   in, 
           FILE*   out, 
           int     only_longest, 
           int     debug);
-
-int alloc_string(String *s);
-
-/* Check if indice i is accessible (< max_size-1) or reallocate until it is.  */
-int string_check(String *s, size_t i);
-
-char *realloc_string(String *s);
 
 int read_input(FILE* in, String* s, int* iter);
 
